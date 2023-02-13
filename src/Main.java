11e643bd85221443781ad398f7afc24f582772e6
Perfight <m0nter1327@mail.ru>
2023-02-13 11:56:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введите значение x: ");
+        double x = in.nextDouble();
+        System.out.print("Введите количество: ");
+        int size = in.nextInt();
+
+        double result = x;
+        for (int n = 1; n <= size; n++) {
+            double a = 1;
+            for (int i = 1; i <= (2 * n - 1); i++) {
+                a *= i;
+            }
+            double b = 1;
+            for (int i = 2; i <= (2 * n + 1); i += 2) {
+                b *= i;
+            }
+            result += a * Math.pow(x, (2 * n + 1)) / b;
+        }
+        System.out.println("Результат: " + result);
+
+        }
+
+    }
